ebeedf31bb21e821d9270c7f14dffb41949d5c16
WoobinHwang <dnqls0326@naver.com>
2023-06-17 11:55:03 +0900

    base setting

--- cgpt/src/main/java/com/cround/cgpt/MainController.java
+++ cgpt/src/main/java/com/cround/cgpt/MainController.java
@@ -15,14 +15,33 @@ public class MainController {
 
 	private final MainService mainService;
 	
-	@GetMapping("/")
-	public String mainpage(Model model) {
+	@GetMapping("/help")
+	public String helpPage(Model model) {
 		// TODO Auto-generated method stub
 		String value = this.mainService.returnValue();
 		
 		// key라는 이름으로 value라는 값을 model에 담아서 프론트로 전송 
 		model.addAttribute("key", value);
+		return "HelpPage";
+	}
+	
+	@GetMapping("/")
+	public String mainPage() {
+		// TODO Auto-generated method stub
 		return "MainPage";
 	}
+	
+	@GetMapping("/signup")
+	public String signupPage() {
+		// TODO Auto-generated method stub
+		return "SignupPage";
+	}
+	
+	@GetMapping("/login")
+	public String loginPage() {
+		// TODO Auto-generated method stub
+		return "LoginPage";
+	}
+	
 	
 }
